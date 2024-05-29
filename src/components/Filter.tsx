8fd1c0a77a2879969db274a4e66bc160a09e09a1
Nikita Yeldynov <yeldynov@gmail.com>
2024-05-29 10:53:28 +0300

    + filter ui for listpage

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,7 +1,75 @@
-import React from "react";
+"use client";
+
+import { ChangeEvent } from "react";
 
 const Filter = () => {
-  return <div>Filter</div>;
+  function handleFilterChange(
+    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ): void {
+    console.log("Not implemented.");
+  }
+
+  return (
+    <div className="mt-12 flex justify-between">
+      <div className="flex flex-wrap gap-6">
+        <select
+          name="type"
+          id=""
+          className="rounded-2xl bg-[#EBEDED] px-4 py-2 text-xs font-medium"
+          onChange={handleFilterChange}
+        >
+          <option>Type</option>
+          <option value="physical">Physical</option>
+          <option value="digital">Digital</option>
+        </select>
+        <input
+          type="text"
+          name="min"
+          placeholder="min price"
+          className="w-24 rounded-2xl pl-2 text-xs ring-1 ring-gray-400"
+          onChange={handleFilterChange}
+        />
+        <input
+          type="text"
+          name="max"
+          placeholder="max price"
+          className="w-24 rounded-2xl pl-2 text-xs ring-1 ring-gray-400"
+          onChange={handleFilterChange}
+        />
+        {/* TODO: Filter Categories */}
+        <select
+          name="cat"
+          className="rounded-2xl bg-[#EBEDED] px-4 py-2 text-xs font-medium"
+          onChange={handleFilterChange}
+        >
+          <option>Category</option>
+          <option value="">New Arrival</option>
+          <option value="">Popular</option>
+        </select>
+        <select
+          name=""
+          id=""
+          className="rounded-2xl bg-[#EBEDED] px-4 py-2 text-xs font-medium"
+        >
+          <option>All Filters</option>
+        </select>
+      </div>
+      <div className="">
+        <select
+          name="sort"
+          id=""
+          className="rounded-2xl bg-white px-4 py-2 text-xs font-medium ring-1 ring-gray-400"
+          onChange={handleFilterChange}
+        >
+          <option>Sort By</option>
+          <option value="asc price">Price (low to high)</option>
+          <option value="desc price">Price (high to low)</option>
+          <option value="asc lastUpdated">Newest</option>
+          <option value="desc lastUpdated">Oldest</option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default Filter;
